e05f4ef2dbba2a0109985f2ea0341bf601e1f2b1
mblandonp <101910407+mblandonp@users.noreply.github.com>
2022-04-01 20:27:08 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -21,8 +21,18 @@ def pregunta_01():
     214
 
     """
-    x = 214
-    return x
+    
+    # Carga el archivo
+    #
+    data = open("data.csv", 'r').readlines()
+    data = [line.replace("\n", "") for line in data] 
+    data = [line.split("\t") for line in data] 
+
+    segcol = [int(row[1]) for row in data]
+    suma = sum(segcol)
+    print(suma)  
+
+    return suma
 
 
 def pregunta_02():
